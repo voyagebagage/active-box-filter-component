4f88b4f7e280c70ab1a8004a47df406b74e85a09
Olivier Frugier <voyagebagageao@gmail.com>
2021-07-12 20:50:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([
+    { type: "Vegan", isActive: true },
+    { type: "Vegetarian", isActive: true },
+    { type: "Veg-Options", isActive: true },
+    { type: "Shop", isActive: true },
+    { type: "Pro", isActive: true },
+  ]);
+
+  const handleCheck = (index) => {
+    let toggle = [...list];
+    toggle[index].isActive = !toggle[index].isActive;
+    setList(toggle);
+  };
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    <div className="container">
+      {list.map((category, index) => {
+        console.log(list);
+        return (
+          <div
+            className={category.isActive ? "buttons active" : "buttons"}
+            key={index}
+            onClick={() => handleCheck(index)}
+          >
+            {category.type}
+          </div>
+        );
+      })}
     </div>
   );
 }
